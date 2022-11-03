f23a540919af3f08fc248b34c10eecffa2517f1f
Matt Pahuta <mhpahuta@gmail.com>
2022-11-03 15:11:27 -0700

    Add payment form modal, confirm message, related functions

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { menuArray } from './data.js'
 
-
-let ordersArray = []
+// array to hold current orders
+let ordersArray = [];
 
 document.addEventListener('click', (e) => {
   
@@ -16,16 +16,86 @@ document.addEventListener('click', (e) => {
     document.getElementById('order-list').innerHTML = renderCartHtml(ordersArray) // re-render cart
     getTotal(ordersArray)
     console.log(ordersArray) // debug
-  }
-  
+  } else if (e.target.id === 'submit-order') {
+    console.log('Getting the payment modal...')
+    document.getElementById('payment-modal').style.display = 'block';
+  } else if (e.target.id === 'modal-close-btn') {
+    document.getElementById('payment-modal').style.display = 'none';
+  } 
 
 })
+
+const paymentForm = document.getElementById('payment-form');
+
+paymentForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log('payment submitted');
+  document.getElementById('payment-modal').style.display = 'none'; // hide payment modal
+  document.getElementById('cart').style.display = 'none';
+  const paymentFormData = new FormData(paymentForm)
+  const firstName = paymentFormData.get('firstName');
+
+  const orderConfirmed =  document.getElementById('order-confirm');
+  orderConfirmed.style.display = 'block'
+  orderConfirmed.innerHTML = `
+  <p>Thanks, ${firstName}! Your order is on the way!</p>
+  `
+
+  console.log(`Thanks, ${firstName}! Your order is on the way.`)
+  // hide the cart
+  // clear the ordersArray
+  // update the total
+
+
+
+
+})
+
+// consentForm.addEventListener('submit', function(e){
+//   e.preventDefault()
+  
+//   const consentFormData = new FormData(consentForm)
+//   const fullName = consentFormData.get('fullName')
+  
+//   modalText.innerHTML = `
+//   <div class="modal-inner-loading">
+//       <img src="images/loading.svg" class="loading">
+//       <p id="upload-text">Uploading your data to the dark web...</p>
+//   </div>` 
+  
+//   setTimeout(function(){
+//       document.getElementById('upload-text').innerText = `
+//       Making the sale...`
+//   }, 1500)
+  
+  
+//   setTimeout(function(){
+//       document.getElementById('modal-inner').innerHTML = `
+//       <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+//       <p>We just sold the rights to your eternal soul.</p>
+//       <div class="idiot-gif">
+//           <img src="images/pirate.gif">
+//       </div>
+//   `
+//   modalCloseBtn.disabled = false
+//   }, 3000)
+
+// }) 
+
+
+
+document.getElementById('pay-btn')
 
 // get/update cart total, display total
 function getTotal(items) { // take in array of items ordered
   let total = 0;
   items.forEach(item => total += item.price) // loop through array, add prices
   document.getElementById('total-amount').textContent = `$${total}`;
+}
+
+// 
+function handlePayment(e) {
+  e.prevent
 }
 
 
@@ -51,7 +121,7 @@ function handleAddMenuItem(menuId) { // pass in data-add value
 // Render the ordered items onto the page
 function renderCartHtml(ordersArray) { 
   if (!ordersArray.length) { // check if the orders array is empty
-    document.getElementById('cart').style.display = 'none';
+    document.getElementById('cart').style.display = 'none'; // hide if there's no orders
   }
 
   let orderHtml = ``;
