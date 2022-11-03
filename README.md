93233fd8f840be856815b30725de12086e9b2e80
Matt Pahuta <mhpahuta@gmail.com>
2022-11-03 15:58:31 -0700

    Update readme, general code cleanup

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Scrimba - Restaurant Ordering app
 
-This is a vanilla JavaScript project from the Scrimba Frontend Engineer Career Path curriculum. 
+This is a mockup of a restaurant ordering app built with vanilla JavaScript, part of the Scrimba Frontend Engineer Career Path curriculum. Design by Scrimba.
 
 ## Overview
 
@@ -11,3 +11,9 @@ Users should be able to:
 - Add and remove menu items from their order  
 - See a payment modal with form inputs to complete order
 - Receive an order complete/confirmation message
+
+### Future Enhancements
+
+- Change theme of app (type of products)
+- Offer combo discount (15% off certain items purchased together)
+- Prompt user for experience rating after purchase
--- js/app.js
+++ js/app.js
@@ -1,23 +1,20 @@
-import { menuArray } from './data.js'
+import { menuArray } from './data.js' // import menu data
 
 // array to hold current orders
 let ordersArray = [];
 
+// general event listener
 document.addEventListener('click', (e) => {
   
   if (e.target.dataset.add) { // if add button clicked
-    console.log(typeof e.target.dataset.add) // debug
     document.getElementById('cart').style.display = 'block'; // show the shopping cart
     handleAddMenuItem(e.target.dataset.add) // call handleAddMenuItem
     getTotal(ordersArray) // call getTotal to update total
   } else if (e.target.dataset.remove) {
-    console.log(e.target.dataset.remove) // debug
     ordersArray.splice(e.target.dataset.remove, 1); // remove item from cart
     document.getElementById('order-list').innerHTML = renderCartHtml(ordersArray) // re-render cart
     getTotal(ordersArray)
-    console.log(ordersArray) // debug
   } else if (e.target.id === 'submit-order') {
-    console.log('Getting the payment modal...')
     document.getElementById('payment-modal').style.display = 'block';
   } else if (e.target.id === 'modal-close-btn') {
     document.getElementById('payment-modal').style.display = 'none';
@@ -25,66 +22,36 @@ document.addEventListener('click', (e) => {
 
 })
 
+// Payment form listener and info
 const paymentForm = document.getElementById('payment-form');
 
 paymentForm.addEventListener('submit', (e) => {
   e.preventDefault()
-  console.log('payment submitted');
   document.getElementById('payment-modal').style.display = 'none'; // hide payment modal
-  document.getElementById('cart').style.display = 'none';
-  const paymentFormData = new FormData(paymentForm)
+  document.getElementById('cart').style.display = 'none'; // hide shopping cart
+  const paymentFormData = new FormData(paymentForm); 
   const firstName = paymentFormData.get('firstName');
-
+  // handle confirmation message:
   const orderConfirmed =  document.getElementById('order-confirm');
   orderConfirmed.style.display = 'block'
   orderConfirmed.innerHTML = `
   <p>Thanks, ${firstName}! Your order is on the way!</p>
   `
-
-  console.log(`Thanks, ${firstName}! Your order is on the way.`)
-  // hide the cart
-  // clear the ordersArray
-  // update the total
-
-
-
+  // wait 3 seconds then reset the cart, hide message
+  setTimeout(() => {
+    resetCart(); // reset the cart
+    orderConfirmed.style.display = 'none'
+  }, 3000)
 
 })
 
-// consentForm.addEventListener('submit', function(e){
-//   e.preventDefault()
-  
-//   const consentFormData = new FormData(consentForm)
-//   const fullName = consentFormData.get('fullName')
-  
-//   modalText.innerHTML = `
-//   <div class="modal-inner-loading">
-//       <img src="images/loading.svg" class="loading">
-//       <p id="upload-text">Uploading your data to the dark web...</p>
-//   </div>` 
-  
-//   setTimeout(function(){
-//       document.getElementById('upload-text').innerText = `
-//       Making the sale...`
-//   }, 1500)
-  
-  
-//   setTimeout(function(){
-//       document.getElementById('modal-inner').innerHTML = `
-//       <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
-//       <p>We just sold the rights to your eternal soul.</p>
-//       <div class="idiot-gif">
-//           <img src="images/pirate.gif">
-//       </div>
-//   `
-//   modalCloseBtn.disabled = false
-//   }, 3000)
-
-// }) 
-
-
-
-document.getElementById('pay-btn')
+// Reset the shopping cart and payment form
+function resetCart() {
+  paymentForm.reset(); // reset the form fields
+  ordersArray.length = 0; // clear the ordersArray
+  getTotal(ordersArray); // update the total
+  renderCartHtml(ordersArray);
+}
 
 // get/update cart total, display total
 function getTotal(items) { // take in array of items ordered
@@ -93,30 +60,17 @@ function getTotal(items) { // take in array of items ordered
   document.getElementById('total-amount').textContent = `$${total}`;
 }
 
-// 
-function handlePayment(e) {
-  e.prevent
-}
-
-
 // Handle add item to order button
 function handleAddMenuItem(menuId) { // pass in data-add value
   const selectedItem = Number(menuId) // convert menuId to number
-
-  console.log('Item ID passed in: ', menuId); // debug
-
   for (let item of menuArray) {
     if (item.id === selectedItem) {
       ordersArray.push(item)
     }
   }
-
-  console.log('Current order array: ', ordersArray) // debug
   // render the current cart:
-  document.getElementById('order-list').innerHTML = renderCartHtml(ordersArray)
+  document.getElementById('order-list').innerHTML = renderCartHtml(ordersArray);
 }
-
-
 
 // Render the ordered items onto the page
 function renderCartHtml(ordersArray) { 
@@ -137,7 +91,7 @@ function renderCartHtml(ordersArray) {
   return orderHtml;
 }
 
-// Build menu item Html
+// Build menu item HTML
 function getMenuHtml() {
   let menuHtml = ``;
 
